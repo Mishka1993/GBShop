90c39c4fd4edef0d4ee2e8b74b4d163c847d79df
Mikhail <misha220993@gmail.com>
2022-07-28 15:47:34 +0400

    Lesson_7

--- GBShop/App/BusinessLogic/Network/Auth.swift
+++ GBShop/App/BusinessLogic/Network/Auth.swift
@@ -12,8 +12,8 @@ class Auth: AbstractRequestFactory {
     let errorParser: AbstractErrorParser
     let sessionManager: Session
     let queue: DispatchQueue
-    let baseUrl = URL(string: "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/")!
-
+    let baseUrl = URL(string: "https://stormy-lowlands-19985.herokuapp.com/")!
+    
     init(errorParser: AbstractErrorParser, sessionManager: Session, queue: DispatchQueue = DispatchQueue.global(qos: .utility)) {
         self.errorParser = errorParser
         self.sessionManager = sessionManager
@@ -25,9 +25,9 @@ class Auth: AbstractRequestFactory {
 extension Auth {
     struct Login: RequestRouter {
         let baseUrl: URL
-        let method: HTTPMethod = .get
-        let path: String = "login.json"
-
+        let method: HTTPMethod = .post
+        let path: String = "login"
+        
         let login: String
         let password: String
         var parameters: Parameters? {
@@ -50,10 +50,10 @@ extension Auth: AuthRequestFactory {
 extension Auth {
     struct LogOut: RequestRouter {
         let baseUrl: URL
-        let method: HTTPMethod = .get
-        let path: String = "logout.json"
+        let method: HTTPMethod = .post
+        let path: String = "logout"
         let userId: Int
-
+        
         var parameters: Parameters? {
             return [
                 "id_user": userId,
@@ -73,10 +73,10 @@ extension Auth: LogOutRequestFactory {
 extension Auth {
     struct Registration: RequestRouter {
         let baseUrl: URL
-        let method: HTTPMethod = .get
-        let path: String = "registerUser.json"
+        let method: HTTPMethod = .post
+        let path: String = "register"
         let userData: RequestUserData
-
+        
         var parameters: Parameters? {
             return userData.toRequestParam()
         }
@@ -94,10 +94,10 @@ extension Auth: RegistrationRequestFactory {
 extension Auth {
     struct ChangeUserData: RequestRouter {
         let baseUrl: URL
-        let method: HTTPMethod = .get
-        let path: String = "changeUserData.json"
+        let method: HTTPMethod = .post
+        let path: String = "change-user-data"
         let userData: RequestUserData
-
+        
         var parameters: Parameters? {
             return userData.toRequestParam()
         }
